aa47ad5bd6e27861250450a05cbbaaff60b54cbf
Климентий Порошин <klimeniy@gmail.com>
2022-04-19 18:48:25 +0600

    jhyg

--- final_win.py
+++ final_win.py
@@ -1,1 +1,17 @@
 # напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt, QTimer, QTime
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QLineEdit
+app_3 = QApplication([])
+
+
+
+
+
+
+
+
+
+
+win_3.setLayout(main_h_line)
+win_3.show()
+app_3.exec_()
